c9fb2e69a7613756c23c7b8756977cc9bb532d9a
Andrew Jones <ajones@digitalbazaar.com>
2022-06-14 14:37:13 -0400

    DRY up assertions.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -2,27 +2,47 @@
  * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {api} from './helpers.js';
+import {testExchanges} from '../mock.data.js';
+import {
+  shouldError,
+  shouldNotError,
+  shouldHaveVpForStep
+} from './assertions.js';
 
 const exchange = '/exchanges/:exchangeId';
 const exchangeInstance = '/exchange-instances/:exchangeInstanceId';
 
-describe('api', () => {
+describe('API', () => {
   describe(exchange, () => {
-    it('should get a VP from a valid service.', async () => {
+    it('should get a VP from a valid service with only one step.', async () => {
       const path = 'exchanges/oneStep';
       const {response, error, data} = await api.post({path});
-      should.not.exist(error, `Expected path ${path} to not error.`);
-      should.exist(response, `Expected path ${path} to return a response.`);
-      should.exist(data, `Expected path ${path} to return data.`);
+      shouldNotError({response, error, data, path});
+      const {
+        verifiablePresentationRequest
+      } = testExchanges.oneStep.steps.initial;
+      shouldHaveVpForStep({data, path, verifiablePresentationRequest});
+    });
+    it('should get a VP from a valid service with two steps.', async () => {
+      const path = 'exchanges/multiStep';
+      const {response, error, data} = await api.post({path});
+      shouldNotError({response, error, data, path});
+      const {
+        verifiablePresentationRequest
+      } = testExchanges.oneStep.steps.initial;
+      shouldHaveVpForStep({data, path, verifiablePresentationRequest});
     });
     it('should error if service doesn\'t exist.', async () => {
       const path = 'exchanges/nonExistent';
       const {response, error, data} = await api.post({path});
-      should.exist(error, `Expected path ${path} to error.`);
-      should.not.exist(
-        response, `Expected path ${path} to not return a response.`);
-      should.not.exist(data, `Expected path ${path} to not return data.`);
+      shouldError({response, error, data, path});
     });
+    it('should error if service doesn\'t have an initial step.', async () => {
+      const path = 'exchanges/noInitialStep';
+      const {response, error, data} = await api.post({path});
+      shouldError({response, error, data, path});
+    });
+
   });
   describe(`${exchange}/:transactionId`, () => {
 
---
+++ test/mocha/assertions.js
@@ -0,0 +1,26 @@
+export const shouldNotError = ({response, error, data, path}) => {
+  should.not.exist(error, `Expected path ${path} to not error.`);
+  should.exist(response, `Expected path ${path} to return a response.`);
+  should.exist(data, `Expected path ${path} to return data.`);
+};
+
+export const shouldError = ({response, error, data, path}) => {
+  should.exist(error, `Expected path ${path} to error.`);
+  should.not.exist(response, `Expected path ${path} to not return a response.`);
+  should.not.exist(data, `Expected path ${path} to not return data.`);
+};
+
+export const shouldHaveVpForStep = ({
+  data,
+  path,
+  verifiablePresentationRequest
+}) => {
+  data.should.be.an(
+    'object',
+    `Expected data from ${path} to be an object.`
+  );
+  data.should.eql(
+    {verifiablePresentationRequest},
+    `Expected data from ${path} to match Vp from initial step.`
+  );
+}
