a5ce7b66077b86ba9e63dbefa606eef73228507f
Andrew Jones <ajones@digitalbazaar.com>
2022-06-14 12:54:30 -0400

    Add initial tests for :exchangeId.

--- test/mock.data.js
+++ test/mock.data.js
@@ -2,18 +2,42 @@ export const testExchanges = {
   // one is a simple valid exchange
   oneStep: {
     steps: {
-
+      initial: {
+        // this is a one step exchange
+        next: null,
+        verifiablePresentationRequest: {
+          query: []
+        }
+      }
     }
   },
-  // sets up an invalid exchange with no initial step.
+  // sets up an invalid exchange because there is no initial step.
   noInitialStep: {
     steps: {
-
+      notInitial: {
+        // this is a one step exchange
+        next: null,
+        verifiablePresentationRequest: {
+          query: []
+        }
+      }
     }
   },
   multiStep: {
     steps: {
-
+      initial: {
+        // this is a multi step exchange
+        next: 'end',
+        verifiablePresentationRequest: {
+          query: []
+        }
+      },
+      end: {
+        next: null,
+        verifiablePresentationRequest: {
+          query: []
+        }
+      }
     }
   },
   transactionOne: {
