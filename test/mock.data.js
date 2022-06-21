0dc071a1637d031bae23de64f659387a4c47a068
Andrew Jones <ajones@digitalbazaar.com>
2022-06-20 20:00:08 -0400

    Add test for delegate step.

--- test/mock.data.js
+++ test/mock.data.js
@@ -70,9 +70,30 @@ export const testExchanges = {
       }
     }
   },
-  delegateOne: {
+  multiStepDelegated: {
     steps: {
-
+      initial: {
+        // this is a multi step exchange
+        next: 'end',
+        verifiablePresentationRequest: {
+          query: [],
+          interact: {
+            service: [{
+              type: 'UnmediatedPresentationService2021',
+              serviceEndpointBasePath: '/exchanges/'
+            }]
+          }
+        }
+      },
+      end: {
+        next: null,
+        verifiablePresentationRequest: {
+          query: [],
+          interact: {
+            service: []
+          }
+        }
+      }
     }
   }
 };
@@ -104,29 +125,31 @@ export const presentations = {
         proofValue: 'z57ZtkyQJXVYK5UXDhcr4bEF4kkBpCpet9odGxBmeWNCTAwy9bVbeDSGGzDHNveD4pgDJQudCqohptQf1Z4SPqphR'
       }
     }]
-  }
-};
-
-export const zcaps = {
-  one: {
-    '@context': [
-      'https://w3id.org/zcap/v1',
-      'https://w3id.org/security/suites/ed25519-2020/v1'
-    ],
-    id: 'urn:uuid:86f2c11e-9bb4-4f03-b71d-2852c04856ec',
-    controller: 'did:key:z6MkogR2ZPr4ZGvLV2wZ7cWUamNMhpg3bkVeXARDBrKQVn2c',
-    parentCapability: 'urn:zcap:root:https%3A%2F%2Fzcap.example%2Fitems',
-    invocationTarget: 'https://zcap.example/items',
-    expires: '2022-06-20T18:02:40Z',
-    proof: {
-      type: 'Ed25519Signature2020',
-      created: '2022-06-20T17:57:40Z',
-      verificationMethod: 'did:key:z6MktKwz7Ge1Yxzr4JHavN33wiwa8y81QdcMRLXQsrH9T53b#z6MktKwz7Ge1Yxzr4JHavN33wiwa8y81QdcMRLXQsrH9T53b',
-      proofPurpose: 'capabilityDelegation',
-      capabilityChain: [
-        'urn:zcap:root:https%3A%2F%2Fzcap.example%2Fitems'
+  },
+  // contains a zcap
+  two: {
+    '@context': ['https://www.w3.org/2018/credentials/v1'],
+    type: ['VerifiablePresentation'],
+    capability: [{
+      '@context': [
+        'https://w3id.org/zcap/v1',
+        'https://w3id.org/security/suites/ed25519-2020/v1'
       ],
-      proofValue: 'zmGMxhr8Puv1WQFvFBonPaT9VoTeSFbmL5sXX37EQKpdeZbE17eQBc1G6T44bVWSitP2dg6cSCjW8AN2LtS3byNL'
-    }
+      id: 'urn:uuid:86f2c11e-9bb4-4f03-b71d-2852c04856ec',
+      controller: 'did:key:z6MkogR2ZPr4ZGvLV2wZ7cWUamNMhpg3bkVeXARDBrKQVn2c',
+      parentCapability: 'urn:zcap:root:https%3A%2F%2Fzcap.example%2Fitems',
+      invocationTarget: 'https://zcap.example/items',
+      expires: '2022-06-20T18:02:40Z',
+      proof: {
+        type: 'Ed25519Signature2020',
+        created: '2022-06-20T17:57:40Z',
+        verificationMethod: 'did:key:z6MktKwz7Ge1Yxzr4JHavN33wiwa8y81QdcMRLXQsrH9T53b#z6MktKwz7Ge1Yxzr4JHavN33wiwa8y81QdcMRLXQsrH9T53b',
+        proofPurpose: 'capabilityDelegation',
+        capabilityChain: [
+          'urn:zcap:root:https%3A%2F%2Fzcap.example%2Fitems'
+        ],
+        proofValue: 'zmGMxhr8Puv1WQFvFBonPaT9VoTeSFbmL5sXX37EQKpdeZbE17eQBc1G6T44bVWSitP2dg6cSCjW8AN2LtS3byNL'
+      }
+    }]
   }
 };
