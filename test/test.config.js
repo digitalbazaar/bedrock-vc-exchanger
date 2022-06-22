af871c95b640584170d0a89b8179c1c3d557c090
Andrew Jones <ajones@digitalbazaar.com>
2022-06-22 15:24:46 -0400

    Sort imports by all, multiple, single, none & then alphabetize.

--- test/test.config.js
+++ test/test.config.js
@@ -2,11 +2,11 @@
  * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {config} from '@bedrock/core';
-import '@bedrock/https-agent';
-import '@bedrock/mongodb';
-import {testExchanges} from './mock.data.js';
 import {fileURLToPath} from 'node:url';
 import path from 'node:path';
+import {testExchanges} from './mock.data.js';
+import '@bedrock/https-agent';
+import '@bedrock/mongodb';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
