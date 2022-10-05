32f114889efb7d95d2bb79bdbfe3b6bab848133d
Andrew Jones <ajones@digitalbazaar.com>
2022-10-05 13:41:29 -0400

    Add initial schemas.

---
+++ schemas/index.js
@@ -0,0 +1,83 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+const constants = {
+  CREDENTIALS_CONTEXT_V1_URL: 'https://www.w3.org/2018/credentials/v1',
+  VERIFIABLE_PRESENTATION: 'VerifiablePresentation'
+};
+
+const verifiablePresentation = {
+  title: 'Verifiable Presentation',
+  type: 'object',
+  required: [
+    '@context',
+    'type'
+  ],
+  properties: {
+    '@context': {
+      title: 'JSON-LD context',
+      description: 'A JSON-LD Context',
+      anyOf: [{
+        const: constants.CREDENTIALS_CONTEXT_V1_URL
+      }, {
+        type: 'object'
+      }, {
+        type: 'array',
+        items: [{const: constants.CREDENTIALS_CONTEXT_V1_URL}],
+        additionalItems: true
+      }]
+    },
+    type: {
+      title: 'JSON-LD type',
+      description: 'A JSON-LD Type',
+      anyOf: [{
+        const: constants.VERIFIABLE_PRESENTATION
+      }, {
+        type: 'array',
+        items: [{const: constants.VERIFIABLE_PRESENTATION}],
+        additionalItems: true
+      }]
+    },
+    verifiableCredential: {
+      title: 'Verifiable Credential',
+      anyOf: [{
+        type: 'object',
+      }, {
+        type: 'array',
+        items: {type: 'object'}
+      }]
+    },
+    proof: {type: 'object'}
+  }
+};
+
+const object = {
+  title: 'An object',
+  description: 'An object',
+  type: 'object'
+};
+
+const vpRequestWithQuery = {
+  title: 'VP Request With Query',
+  description: 'A Verifiable Presentations Request with a Query',
+  type: 'object',
+  additionalItems: true,
+  query: {
+    title: 'VP Request Query',
+    type: 'array',
+    items: {type: 'object'}
+  }
+};
+
+const initiateExchange = {
+  title: 'Initiate Exchange Request',
+  anfOf: [object, vpRequestWithQuery]
+};
+
+export const schemas = [{
+  type: constants.VERIFIABLE_PRESENTATION,
+  schema: verifiablePresentation
+}, {
+  type: 'initiateExchange',
+  schema: initiateExchange
+}];
